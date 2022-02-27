5a1eedec9801f512be2c85680226c1c475902741
ishan <ishan9@gmail.com>
2022-02-27 19:18:13 +0530

    first commit by ishan

---
+++ app/src/main/java/com/ishan/imageeditortask/EditActivity.kt
@@ -0,0 +1,13 @@
+package com.ishan.imageeditortask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+
+    }
+}
---
+++ app/src/main/java/com/ishan/imageeditortask/HomeActivity.kt
@@ -0,0 +1,185 @@
+package com.ishan.imageeditortask
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
+
+class HomeActivity : AppCompatActivity() {
+
+    companion object {
+        private const val CAMERA = 1
+        private const val GALLERY = 2
+        private const val IMAGE_DIRECTORY = "ImageEditorTask"
+    }
+    lateinit var preview : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        preview = findViewById(R.id.preview)
+
+        val openGallery = findViewById<FloatingActionButton>(R.id.openGallery)
+        openGallery.setOnClickListener {
+            choosePhotoFromGallery()
+        }
+
+        val openCamera = findViewById<FloatingActionButton>(R.id.openCamera)
+        openCamera.setOnClickListener {
+            takePhotoFromCamera()
+        }
+
+        val edit = findViewById<FloatingActionButton>(R.id.edit)
+        edit.setOnClickListener {
+            val intent = Intent(this , EditActivity::class.java)
+
+            startActivity(intent)
+        }
+
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                if (data != null) {
+                    val contentURI = data.data
+                    try {
+                        // Here this is used to get an bitmap from URI
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                        saveImageToInternalStorage(selectedImageBitmap)
+                        preview!!.setImageBitmap(selectedImageBitmap) // Set the selected image from GALLERY to imageView.
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                // TODO (Step 7: Camera result will be received here.)
+            } else if (requestCode == CAMERA) {
+                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap // Bitmap from camera
+                saveImageToInternalStorage(thumbnail)
+                preview!!.setImageBitmap(thumbnail) // Set to the imageView.
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Cancelled", "Cancelled")
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                    // Here after all the permission are granted launch the gallery to select and image.
+                    if (report!!.areAllPermissionsGranted()) {
+
+                        // TODO(Step 1 : Adding an image selection code here from Gallery or phone storage.)
+                        // START
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+                        startActivityForResult(galleryIntent, GALLERY)
+                        // END
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+    private fun takePhotoFromCamera() {
+
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    // Here after all the permission are granted launch the CAMERA to capture an image.
+                    if (report!!.areAllPermissionsGranted()) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(intent, CAMERA)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+
+    private fun saveImageToInternalStorage(bitmap: Bitmap) : Uri {
+        val wrapper = ContextWrapper(applicationContext)
+        var file = wrapper.getDir(IMAGE_DIRECTORY , Context.MODE_PRIVATE)
+        file = File(file , " ${UUID.randomUUID()}.jpg")
+        try {
+            val stream : OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG , 100 , stream)
+            stream.flush()
+            stream.close()
+        }catch ( e : IOException){
+            e.printStackTrace()
+        }
+        return Uri.parse(file.absolutePath)
+    }
+
+     private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("permission de bsdk")
+            .setPositiveButton("Go to Settings"){_,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package" , packageName , null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e : Exception ){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){ dialog , _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+
+}
